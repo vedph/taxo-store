15ebe0e695469910cbc11260e77fdfad3e5ce5a8
Myrmex <dfusi@hotmail.com>
2026-01-26 09:31:02 +0100

    Part seeder tests

---
+++ Cadmus.Seed.TaxoStore.Parts.Test/TaxoStorePartSeederTest.cs
@@ -0,0 +1,57 @@
+﻿using Cadmus.Core;
+using Cadmus.Seed;
+using Cadmus.TaxoStore.Parts;
+using Fusi.Tools;
+using Fusi.Tools.Configuration;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Cadmus.Seed.TaxoStore.Parts.Test;
+
+public sealed class TaxoStorePartSeederTest
+{
+    private static readonly PartSeederFactory _factory =
+        TestHelper.GetFactory();
+    private static readonly SeedOptions _seedOptions =
+        _factory.GetSeedOptions();
+    private static readonly IItem _item =
+        _factory.GetItemSeeder().GetItem(1, "facet");
+
+    [Fact]
+    public void TypeHasTagAttribute()
+    {
+        Type t = typeof(TaxoStoreNodesPartSeeder);
+        TagAttribute? attr = t.GetTypeInfo().GetCustomAttribute<TagAttribute>();
+        Assert.NotNull(attr);
+        Assert.Equal("seed.it.vedph.taxo-store-nodes", attr!.Tag);
+    }
+
+    [Fact]
+    public void Seed_Ok()
+    {
+        TaxoStoreNodesPartSeeder seeder = new();
+        seeder.SetSeedOptions(_seedOptions);
+        seeder.Configure(new TaxoStoreNodesPartSeederOptions
+        {
+            TreeIds = ["animals"],
+            Nodes =
+            [
+                new StringPair("dog", "animals/cat"),
+                new StringPair("dog", "animals/dog"),
+            ]
+        });
+
+        IPart? part = seeder.GetPart(_item, null, _factory);
+
+        Assert.NotNull(part);
+
+        TaxoStoreNodesPart? p = part as TaxoStoreNodesPart;
+        Assert.NotNull(p);
+
+        TestHelper.AssertPartMetadata(p!);
+
+        Assert.NotNull(p!.TreeId);
+        Assert.NotEmpty(p!.NodeIds);
+    }
+}
---
+++ Cadmus.Seed.TaxoStore.Parts.Test/TestHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using Cadmus.Core;
+using Cadmus.Core.Config;
+using Fusi.Microsoft.Extensions.Configuration.InMemoryJson;
+using System.Reflection;
+using System.Text;
+using Microsoft.Extensions.Hosting;
+using Cadmus.TaxoStore.Parts;
+
+namespace Cadmus.Seed.TaxoStore.Parts.Test;
+
+static internal class TestHelper
+{
+    static public Stream GetResourceStream(string name)
+    {
+        ArgumentNullException.ThrowIfNull(name);
+
+        return Assembly.GetExecutingAssembly().GetManifestResourceStream(
+            $"Cadmus.Seed.TaxoStore.Parts.Test.Assets.{name}")!;
+    }
+
+    static public string LoadResourceText(string name)
+    {
+        ArgumentNullException.ThrowIfNull(name);
+
+        using StreamReader reader = new(GetResourceStream(name),
+            Encoding.UTF8);
+        return reader.ReadToEnd();
+    }
+
+    private static IHost GetHost(string config)
+    {
+        // map
+        TagAttributeToTypeMap map = new();
+        map.Add(
+        [
+            // Cadmus.Core
+            typeof(StandardItemSortKeyBuilder).Assembly,
+            // Cadmus.TaxoStore.Parts
+            typeof(TaxoStoreNodesPart).Assembly
+        ]);
+
+        return new HostBuilder().ConfigureServices((hostContext, services) =>
+        {
+            PartSeederFactory.ConfigureServices(services,
+                new StandardPartTypeProvider(map),
+                    // Cadmus.Seed.TaxoStore.Parts
+                    typeof(TaxoStoreNodesPartSeeder).Assembly);
+        })
+            // extension method from Fusi library
+            .AddInMemoryJson(config)
+            .Build();
+    }
+
+    static public PartSeederFactory GetFactory()
+    {
+        return new PartSeederFactory(GetHost(LoadResourceText("SeedConfig.json")));
+    }
+
+    static public void AssertPartMetadata(IPart part)
+    {
+        Assert.NotNull(part.Id);
+        Assert.NotNull(part.ItemId);
+        Assert.NotNull(part.UserId);
+        Assert.NotNull(part.CreatorId);
+    }
+}
